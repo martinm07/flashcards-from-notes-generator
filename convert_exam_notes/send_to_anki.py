2cb040b6e41e6dc25ab10eada7cbdfa9f7254905
martinm07 <martin.github07@gmail.com>
2026-04-17 00:43:54 +0100

    Update Anki integration
    - Make it insert cards into a specific subdeck based on subject area
      (Maths, CS or Physics).
    - Make it use a new field defined on PastPaperNote card types,
      "OriginalNote", to store the original note rather than do it in the
      "Back" field.

--- convert_exam_notes/send_to_anki.py
+++ convert_exam_notes/send_to_anki.py
@@ -3,20 +3,25 @@ import markdown
 
 def send_card_to_anki(card):
     # 2. Push each card to Anki via AnkiConnect
-    final_back = card["back"] + "<div class='original-note'><span>" + markdown.markdown(card["original_note"]) + "</span></div>"
+    final_back = card["back"]
 
-    # print("Live update!")
+    tags = card.get("tags", [])
+    if "Maths" in tags: deckName = "PastPaperNotes::Maths"
+    elif "Computer Science" in tags: deckName = "PastPaperNotes::CS"
+    elif "Physics" in tags: deckName = "PastPaperNotes::Physics"
+    else: deckName = "PastPaperNotes"
 
     payload = {
         "action": "addNote",
         "version": 6,
         "params": {
             "note": {
-                "deckName": "PastPaperNotes",  # change to your deck
+                "deckName": deckName,  # change to your deck
                 "modelName": "PastPaperNote",
                 "fields": {
                     "Front": card["front"],
-                    "Back": final_back
+                    "Back": final_back,
+                    "OriginalNote": markdown.markdown(card["original_note"])
                 },
                 "tags": [tag.replace(" ", "_") for tag in card.get("tags", [])],
                 "options": {"allowDuplicate": False}
